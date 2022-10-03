8503f3aa771390ad344138ac55fc4a0f3a0e3796
anggitfirman <103194724+anggitfirman@users.noreply.github.com>
2022-10-03 20:23:19 +0700

    Add files via upload

---
+++ Lat1 Anggit Firmansyah.php
@@ -0,0 +1,24 @@
+<?php
+//class mobil
+Class Mobil{
+      public $nama;
+      public $merk;
+	  public $a;
+  
+      function getInfo(){
+            echo "Nama mobil: ".$this->nama."<br/>";
+            echo "Merk: ".$this->merk."<br/>";
+			echo "warna: ".$this->a."<br/>";
+      }
+}
+
+//bagian main
+$ferari=new Mobil();
+$ferari->nama="xxx";
+$ferari->merk="aaa";
+$ferari->a="hitam";
+
+$ferari->getInfo();
+?>
+
+Jadi bisa disimpulkan bahwa method overload merupakan kumpulan beberapa method dengan nama yang sama namun memiliki parameter yang berbeda
